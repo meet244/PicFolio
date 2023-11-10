f6aca1db329ce8651bd1d4a31c83544356bae1ce
Meet Patel <meet2005pokar@gmail.com>
2023-11-10 21:17:10 +0530

    image upload success

--- main/index.py
+++ main/index.py
@@ -2,40 +2,66 @@ from flask import Flask, request
 from PIL import Image
 from PIL.ExifTags import TAGS
 import os
+import uuid
+import sqlite3
+from datetime import datetime
+
+# create the database file
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS asset (
+name TEXT PRIMARY KEY UNIQUE NOT NULL,
+date DateTime NOT NULL,
+isphoto BOOL NOT NULL DEFAULT TRUE
+);
+"""
+cursor.execute(create_table_sql)
+connection.commit()
+connection.close()
+
 
 app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
-def upload_image():
+def upload():
     # Check if the post request has the file part
-    if 'image' not in request.files:
+    if 'file' not in request.files:
         return 'No image uploaded'
 
-    image_file = request.files['image']
+    file = request.files['file']
+    format = file.filename.split('.')[-1]
+    isPhoto = True
 
-    # Save the image to a temporary file
-    image_filename = image_file.filename
-    image_path = os.path.join(image_filename)
-    image_file.save(image_path)
+    #check image format
+    if format in ['jpg', 'jpeg','webp', 'png']:
+        pass
+    elif format in ['mp4', 'mkv']:
+        isPhoto = False
+    else:
+        return 'Invalid format'
 
-    # Open the image and get the creation date
-    with Image.open(image_path) as image:
-        # creation_date = image.getexif()
-        exif_data = image.getexif()
+    # create a folder of today's year inside that folder of month and inside that folder of date then save the image there
+    id = str(uuid.uuid4())
+    id = uuid.uuid4().hex
+    today = datetime.now()
+    today_folder = os.path.join(str(today.year), str(today.month), str(today.day))
 
-        exif_info = {TAGS.get(k, k): v for k, v in exif_data.items()}
-        for key, value in exif_info.items():
-            print(f"{key}: {value}")
+    # inser data to db
+    add_row = f"INSERT INTO asset (name, date, isphoto) VALUES (?, ?, ?);"
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    # check_table(cursor)
+    cursor.execute(add_row, (id, today.strftime('%Y-%m-%d %H:%M:%S'), isPhoto))
+    connection.commit()
+    connection.close()
 
-        # print(creation_date)
-        # creation_date = image.getexif().get(IMAGE_CREATION_DATE)
-        # print(f'Image creation date: {creation_date}')
-
-    # Save the image to a permanent file
-    saved_image_path = os.path.join(image_filename)
-    os.rename(image_path, saved_image_path)
+    os.makedirs(today_folder, exist_ok=True)
+    file.save(os.path.join(today_folder, id+'.'+format))
 
     return 'Image uploaded and saved successfully'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
